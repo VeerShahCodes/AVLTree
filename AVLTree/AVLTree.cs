77520bd2b6ddc9126677d661b3531deb9dfdee2f
Veer.Shah <shahveer1213@gmail.com>
2024-12-15 15:41:27 -0800

    im very confused and i am lost

---
+++ AVLTree/AVLTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTree
+{
+    public class AVLTree<T>
+        where T : IComparable<T>
+    {
+        public Node<T> Root;
+        public int balance;
+        public void Add(T value)
+        {
+            Node<T> current = Root;
+            if(Root == null)
+            {
+                Root = new Node<T>(value);
+            }
+
+        }
+
+        public void Delete(T value)
+        {
+
+        }
+    }
+}
---
+++ AVLTree/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVLTree
+{
+    public class Node<T>
+        where T : IComparable<T>
+    {
+        public Node<T> leftChild;
+        public Node<T> rightChild;
+        public T value;
+        public int height;
+        public Node(T val)
+        {
+            value = val;
+        }
+    }
+}
