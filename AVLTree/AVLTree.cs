66d2999e0fc0d25af61391c93cdd79fd31dcec49
VeerShahCodes <shahveer1213@gmail.com>
2024-12-26 17:37:27 -0800

    I remade my add function, rotate functions, and made a balance function in order to simplify debugging and understand my code better. I was able to fix my mistakes and It works for basic cases right now.

--- AVLTree/AVLTree.cs
+++ AVLTree/AVLTree.cs
@@ -17,69 +17,79 @@ namespace avlTree
         {
 
         }
-        public void Add(T value, Node<T> current) //values are getting deleted after rotation
-        {
-            
-            if (Root == null)
-            {
-                Root = new Node<T>(value);
-                return;
-            }
 
+        public void Add(T value)
+        {
+            Root = Add(value, Root);
+        }
+        public Node<T> Add(T value, Node<T> current) //values are getting deleted after rotation
+        {
+
+            if (current == null) return new Node<T>(value);
             if(value.CompareTo(current.Value) < 0)
             {
-                if(current.LeftChild != null)
-                {
-                    Add(value, current.LeftChild);
-                    current.UpdateHeight();
-                    Node<T> returnNode;
-                    if (current.Balance > 1)
-                    {
-                        returnNode = RotateLeft(current);
-                    }
-                    else if (current.Balance < -1)
-                    {
-                        returnNode = RotateRight(current);
-                    }
-                    current.UpdateHeight();
-                }
-                else
-                {
-                    current.LeftChild = new Node<T>(value);
-                    return;
-                }
+                current.LeftChild = Add(value, current.LeftChild);
             }
-            else
+            else if(value.CompareTo(current.Value) > 0)
             {
-                if(current.RightChild != null)
-                {
-                    Add(value, current.RightChild);
-                    current.UpdateHeight();
-                    Node<T> returnNode;
-                    if (current.Balance > 1) //height is being calculated incorrectly which is causing this if statement to not be hit when current is 7.
-                    {
-                        returnNode = RotateLeft(current);
-                        if(returnNode.LeftChild == Root)
-                        {
-                            Root = returnNode;
-                        }
-                    }
-                    else if (current.Balance < -1)
-                    {
-                        returnNode = RotateRight(current);
-                        if (returnNode.LeftChild == Root)
-                        {
-                            Root = returnNode;
-                        }
-                    }
-                    //current.UpdateHeight();
-                }
-                else
-                {
-                    current.RightChild = new Node<T>(value);
-                    return;
-                }
+                current.RightChild = Add(value, current.RightChild);
             }
+            current.UpdateHeight();
+            return Balance(current);
+
+
+            //if(value.CompareTo(current.Value) < 0)
+            //{
+            //    if(current.LeftChild != null)
+            //    {
+            //        Add(value, current.LeftChild);
+            //        current.UpdateHeight();
+            //        Node<T> returnNode;
+            //        if (current.Balance > 1)
+            //        {
+            //            returnNode = RotateLeft(current);
+            //            returnNode.UpdateHeight();
+            //        }
+            //        else if (current.Balance < -1)
+            //        {
+            //            returnNode = RotateRight(current);
+            //            returnNode.UpdateHeight();
+            //        }
+            //        current.UpdateHeight();
+            //    }
+            //    else
+            //    {
+            //        current.LeftChild = new Node<T>(value);
+            //        return;
+            //    }
+            //}
+            //else
+            //{
+            //    if(current.RightChild != null)
+            //    {
+            //        Add(value, current.RightChild);
+            //        current.UpdateHeight();
+            //        Node<T> returnNode;
+            //        if (current.Balance > 1) //height is being calculated incorrectly which is causing this if statement to not be hit when current is 7.
+            //        {
+            //            returnNode = RotateLeft(current);
+            //            if(current == Root) Root = returnNode;
+            //            returnNode.UpdateHeight();
+            //        }
+            //        else if (current.Balance < -1)
+            //        {
+            //            returnNode = RotateRight(current);
+            //            if (current == Root) Root = returnNode;
+            //            returnNode.UpdateHeight();
+            //        }
+            //        current.UpdateHeight();
+            //    }
+            //    else
+            //    {
+            //        current.RightChild = new Node<T>(value);
+            //        return;
+            //    }
+            //}
 
 
         }
@@ -89,39 +99,72 @@ namespace avlTree
 
         }
 
-  
+        public Node<T> Balance(Node<T> current)
+        {
+            if(current.Balance < -1)
+            {
+                //if(current.LeftChild != null)
+                //{
+                //    if (current.LeftChild.Balance < 0)
+                //    {
+                //        current.LeftChild = RotateLeft(current.LeftChild);
+                //    }
+                //}
+
+                return RotateRight(current);
+            }
+            else if(current.Balance > 1)
+            {
+
+                //if(current.RightChild != null)
+                //{
+                //    if (current.RightChild.Balance > 0)
+                //    {
+                //        current.RightChild = RotateRight(current.RightChild);
+                //    }
+                //}
+
+                return RotateLeft(current);
+            }
+            return current;
+        }
         public Node<T> RotateLeft(Node<T> current) 
         {
-            Node<T> rightChild = current.RightChild;
-            Node<T> ogLeftChild = rightChild.LeftChild;
-            Node<T> ogCurrent = current;
-            current = rightChild;
-            rightChild.LeftChild = ogCurrent;
-            ogCurrent.RightChild = null;
-            ogCurrent.LeftChild = ogLeftChild;
-            return rightChild;
+            Node<T> newRoot = current.RightChild;
+            current.RightChild = newRoot.LeftChild; 
+            newRoot.LeftChild = current;
+            current.UpdateHeight();
+            newRoot.UpdateHeight();
+
+            return newRoot;
         }
 
         public Node<T> RotateRight(Node<T> current) 
         {
-            Node<T> leftChild = current.LeftChild;
-            Node<T> ogRightChild = leftChild.RightChild;
-            leftChild.RightChild = current;
-            current.LeftChild = null;
-            current.RightChild = ogRightChild;
-            return leftChild;
+            if (current.LeftChild == null)
+            {
+                return current;
+            }
+            Node<T> newRoot = current.LeftChild;
+            current.LeftChild = newRoot.RightChild;
+            newRoot.RightChild = current;
+            current.UpdateHeight();
+            newRoot.UpdateHeight();
+
+            return newRoot;
         }
 
-        public void BreadthFirstTraversal(Node<T> current)
+        public void DepthFirstTraversal(Node<T> current)
         {
+            if (current == null) return;
             Console.WriteLine("Value: " + current.Value + ", Height: " + current.Height);
             if(current.LeftChild != null)
             {
-                BreadthFirstTraversal(current.LeftChild);
+                DepthFirstTraversal(current.LeftChild);
             }
             if(current.RightChild != null)
             {
-                BreadthFirstTraversal(current.RightChild);
+                DepthFirstTraversal(current.RightChild);
             }
         }
     }
--- AVLTree/Program.cs
+++ AVLTree/Program.cs
@@ -6,12 +6,12 @@
         {
             AVLTree<int> avlTree = new AVLTree<int>();
 
-            avlTree.Add(5, avlTree.Root);
-            avlTree.Add(6, avlTree.Root);
-            avlTree.Add(7, avlTree.Root);
-            avlTree.Add(8, avlTree.Root);
-            avlTree.Add(9, avlTree.Root);
-           avlTree.BreadthFirstTraversal(avlTree.Root);
+            avlTree.Add(5);
+            avlTree.Add(6);
+            avlTree.Add(7);
+            avlTree.Add(8);
+            avlTree.Add(9);
+           avlTree.DepthFirstTraversal(avlTree.Root);
         }
     }
 }
