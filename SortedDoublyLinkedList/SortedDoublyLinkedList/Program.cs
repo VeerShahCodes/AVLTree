4d73318e26b5d609f2b1cf278baf207e26c747b0
VeerShahCodes <shahveer1213@gmail.com>
2025-01-04 20:14:54 -0800

    It doesnt work at all

--- SortedDoublyLinkedList/SortedDoublyLinkedList/Program.cs
+++ SortedDoublyLinkedList/SortedDoublyLinkedList/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            SortedDoublyLinkedList<int> linkedList = new SortedDoublyLinkedList<int>(5);
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                linkedList.Insert(random.Next(100));
+            }
+
+            linkedList.WriteNodes();
         }
     }
 }
--- SortedDoublyLinkedList/SortedDoublyLinkedList/SortedDoublyLinkedList.cs
+++ SortedDoublyLinkedList/SortedDoublyLinkedList/SortedDoublyLinkedList.cs
@@ -18,26 +18,45 @@ namespace SortedDoublyLinkedList
         public void Insert(T val)
         {
             Node<T> current = Head;
-            while(current.Next != null && current.Next.Value.CompareTo(current.Value) > 0)
+            while (current.Next != null && current.Next.Value.CompareTo(current.Value) < 0)
             {
                 current = current.Next;
             }
+            Node<T> newNode = new Node<T>(val);
+
+            if (current.Next == null)
+            {
+                ConnectNodes(current, newNode, null);
+            }
+            else
+            {
+                ConnectNodes(current, newNode, current.Next);
+
+            }
+
 
 
 
         }
         public void ConnectNodes(Node<T> previous, Node<T> newNode, Node<T> next)
         {
+
             previous.Next = newNode;
+
+            
             newNode.Previous = previous;
             newNode.Next = next;
-            next.Previous = newNode;
+            if(next != null)
+            {
+                next.Previous = newNode;
+
+            }
         }
 
         public void WriteNodes()
         {
             Node<T> current = Head;
-            while(current.Next != null)
+            while(current != null)
             {
                 Console.WriteLine(current.Value);
                 current = current.Next;
