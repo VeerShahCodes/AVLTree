9d62e3196924bf730a5fab822414ee5f7a5474ad
VeerShahCodes <shahveer1213@gmail.com>
2025-01-04 19:24:21 -0800

    Made connect nodes helper function for the inserting

---
+++ SortedDoublyLinkedList/SortedDoublyLinkedList/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedDoublyLinkedList
+{
+    public class Node<T>
+        where T : IComparable<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
+
+        public Node(T val)
+        {
+            Value = val;
+        }
+    }
+}
---
+++ SortedDoublyLinkedList/SortedDoublyLinkedList/SortedDoublyLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedDoublyLinkedList
+{
+    public class SortedDoublyLinkedList<T>
+        where T : IComparable<T>
+    {
+        Node<T> Head { get; set; }
+
+        public SortedDoublyLinkedList(T startVal)
+        {
+            Head = new Node<T>(startVal);
+        }
+        public void Insert(T val)
+        {
+            Node<T> current = Head;
+            
+
+        }
+        public void ConnectNodes(Node<T> previous, Node<T> newNode, Node<T> next)
+        {
+            previous.Next = newNode;
+            newNode.Previous = previous;
+            newNode.Next = next;
+            next.Previous = newNode;
+        }
+    }
+}
