dece177c0387b3718d67889d9219019aeb605fad
VeerShahCodes <shahveer1213@gmail.com>
2025-01-04 20:19:40 -0800

    It works now and I finished kind of

--- SortedDoublyLinkedList/SortedDoublyLinkedList/Program.cs
+++ SortedDoublyLinkedList/SortedDoublyLinkedList/Program.cs
@@ -5,14 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            SortedDoublyLinkedList<int> linkedList = new SortedDoublyLinkedList<int>(5);
+            SortedDoublyLinkedList<int> linkedList = new SortedDoublyLinkedList<int>();
             Random random = new Random();
             for (int i = 0; i < 100; i++)
             {
-                linkedList.Insert(random.Next(100));
+                linkedList.Add(random.Next(100));
             }
 
-            linkedList.WriteNodes();
+            linkedList.PrintList();
         }
     }
 }
--- SortedDoublyLinkedList/SortedDoublyLinkedList/SortedDoublyLinkedList.cs
+++ SortedDoublyLinkedList/SortedDoublyLinkedList/SortedDoublyLinkedList.cs
@@ -1,66 +1,128 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SortedDoublyLinkedList
+public class SortedDoublyLinkedList<T> where T : IComparable<T>
 {
-    public class SortedDoublyLinkedList<T>
-        where T : IComparable<T>
+    private class Node
     {
-        Node<T> Head { get; set; }
+        public T Data { get; set; }
+        public Node Next { get; set; }
+        public Node Prev { get; set; }
 
-        public SortedDoublyLinkedList(T startVal)
+        public Node(T data)
         {
-            Head = new Node<T>(startVal);
-        }
-        public void Insert(T val)
-        {
-            Node<T> current = Head;
-            while (current.Next != null && current.Next.Value.CompareTo(current.Value) < 0)
-            {
-                current = current.Next;
-            }
-            Node<T> newNode = new Node<T>(val);
-
-            if (current.Next == null)
-            {
-                ConnectNodes(current, newNode, null);
-            }
-            else
-            {
-                ConnectNodes(current, newNode, current.Next);
-
-            }
-
-
-
-
-        }
-        public void ConnectNodes(Node<T> previous, Node<T> newNode, Node<T> next)
-        {
-
-            previous.Next = newNode;
-
-            
-            newNode.Previous = previous;
-            newNode.Next = next;
-            if(next != null)
-            {
-                next.Previous = newNode;
-
-            }
-        }
-
-        public void WriteNodes()
-        {
-            Node<T> current = Head;
-            while(current != null)
-            {
-                Console.WriteLine(current.Value);
-                current = current.Next;
-            }
+            Data = data;
+            Next = null;
+            Prev = null;
         }
     }
+
+    private Node head;
+    private Node tail;
+    public int Count { get; private set; }
+
+    public SortedDoublyLinkedList()
+    {
+        head = null;
+        tail = null;
+        Count = 0;
+    }
+
+    public void Add(T item)
+    {
+        Node newNode = new Node(item);
+
+        if (head == null) 
+        {
+            head = tail = newNode;
+        }
+        else if (head.Data.CompareTo(item) >= 0) 
+        {
+            newNode.Next = head;
+            head.Prev = newNode;
+            head = newNode;
+        }
+        else
+        {
+            Node current = head;
+            while (current.Next != null && current.Next.Data.CompareTo(item) < 0)
+            {
+                current = current.Next;
+            }
+
+            newNode.Next = current.Next;
+            newNode.Prev = current;
+
+            if (current.Next != null)
+            {
+                current.Next.Prev = newNode;
+            }
+            else 
+            {
+                tail = newNode;
+            }
+
+            current.Next = newNode;
+        }
+
+        Count++;
+    }
+
+    public bool Remove(T item)
+    {
+        Node current = head;
+
+        while (current != null)
+        {
+            if (current.Data.CompareTo(item) == 0)
+            {
+                if (current.Prev != null)
+                {
+                    current.Prev.Next = current.Next;
+                }
+                else 
+                {
+                    head = current.Next;
+                }
+
+                if (current.Next != null)
+                {
+                    current.Next.Prev = current.Prev;
+                }
+                else 
+                {
+                    tail = current.Prev;
+                }
+
+                Count--;
+                return true;
+            }
+
+            current = current.Next;
+        }
+
+        return false;
+    }
+
+    public void PrintList()
+    {
+        Node current = head;
+        while (current != null)
+        {
+            Console.Write(current.Data + " ");
+            current = current.Next;
+        }
+        Console.WriteLine();
+    }
+
+    public void PrintReverse()
+    {
+        Node current = tail;
+        while (current != null)
+        {
+            Console.Write(current.Data + " ");
+            current = current.Prev;
+        }
+        Console.WriteLine();
+    }
 }
+
